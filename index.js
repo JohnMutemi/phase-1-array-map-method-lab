37ea66e88ea945f03768091e889658c50c69aa0a
JohnMutemi <jmtutorsalp@gmail.com>
2024-03-29 18:47:25 +0300

    commit index.js

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+// define a function titleCased that  returns an array with title case tutorial names.
+function titleCased() {
+  return tutorials.map(function(tutorial) {
+    // Split each tutorial title into an array of words
+    const wordsInArray = tutorial.split(' ');
+    // Capitalize the first letter of each word and join them back
+    const upperCasedWords = wordsInArray.map(function(letter) {
+      return letter.charAt(0).toUpperCase() + letter.slice(1);
+    });
+    // Join the capitalized words
+    return upperCasedWords.join(' ');
+  });
 }
+
+// invoke the function
+console.log(titleCased());
